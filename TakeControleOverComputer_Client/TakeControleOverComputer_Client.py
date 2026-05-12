214bf51642c941a876c9a14f9514a8ab1a8a96e9
Liz Lioubimov <Lizadmin@gmail.com>
2026-05-12 14:29:30 +0300

    Finished the project

--- TakeControleOverComputer_Client/TakeControleOverComputer_Client.py
+++ TakeControleOverComputer_Client/TakeControleOverComputer_Client.py
@@ -1,53 +1,86 @@
-from http import client
 import socket
 from time import sleep
 import pyautogui
 import io
-import turtle
-from pynput.mouse import Button, Controller
+import pynput
 import screeninfo
+import tkinter as tk
+import threading
 
 "3 ports are needed"
+CHAR_SIZE_IN_BYTES = 1
+INT_SIZE_IN_BYTES = 4
+STR_CONVERTION = "utf-8"
+INT_CONVERTION = "big"
 
 def connect_to_server(port):
     SERVER_IP = "10.100.102.23"
 
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        soc.connect((SERVER_IP, port))
-    except ConnectionRefusedError:
-        """label_file_name.set("An exception occured while connecting")"""
-        return
+    soc.connect((SERVER_IP, port))
 
     return soc;
 
-def controle_keyboard(soc):
-    pass
-
-def screen_share():
-    SERVER_PORT = 32445
-
+def keyboard_controle():
+    SERVER_PORT = 32444
+    global CHAR_SIZE_IN_BYTES
+    global STR_CONVERTION
+    
     soc = connect_to_server(SERVER_PORT)
+    keyboard = pynput.keyboard.Controller()
+    to_do = "unknown"
+    while True:
+        to_do = soc.recv(CHAR_SIZE_IN_BYTES).decode(STR_CONVERTION)
+        """if shift_pressed:
+            with keyboard.pressed(keyboard.key.shift):
+                press_release_key(soc=soc, keyboard=keyboard, to_do=to_do, shift_pressed=shift_pressed)
+                GGGGGGHHHHHHHGSSSAAAAAXXVVggggAAAADFFFFGGGGGDDDDDddd        \\
+                PPPpppp
+                103FDSSS"""
+        if to_do == 'p':
+            key = soc.recv(INT_SIZE_IN_BYTES)
+            key = int.from_bytes(key, INT_CONVERTION)
+            key = key_from_vk(key)
+            keyboard.press(key)
 
+        if to_do == 'r':
+            key = soc.recv(INT_SIZE_IN_BYTES)
+            key = int.from_bytes(key, INT_CONVERTION)
+            key = key_from_vk(key)
+            keyboard.release(key)
+            
+
+
+def key_from_vk(vk):
+    for special_key in pynput.keyboard.Key:
+        if special_key.value.vk == vk:
+            return special_key
+
+    return pynput.keyboard.KeyCode.from_vk(vk)
+        
+def share_screen():
+    SERVER_PORT = 32445
+    global INT_CONVERTION
+    soc = connect_to_server(SERVER_PORT)
+    
     while True:
         image = pyautogui.screenshot()
         buffer = io.BytesIO()
         image.save(buffer, format='png')
-
-        soc.send(len(buffer.getvalue()).to_bytes(4, byteorder='big'))
-        soc.sendall(buffer.getvalue())
-
-        sleep(0.1)
-
-
+        num = soc.send(len(buffer.getvalue()).to_bytes(INT_SIZE_IN_BYTES, byteorder=INT_CONVERTION))
+        num = soc.send(buffer.getvalue())
+    
 def mouse_controle():
     SERVER_PORT = 32446
     soc = connect_to_server(SERVER_PORT)
     
-    INT_SIZE_IN_BYTES = 4
-    CHAR_SIZE_IN_BYTES = 1
-    client_mouse = Controller()    
-    data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), "big")
+    global INT_SIZE_IN_BYTES
+    global CHAR_SIZE_IN_BYTES
+    global STR_CONVERTION
+    global INT_CONVERTION
+    
+    client_mouse = pynput.mouse.Controller()    
+    data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), INT_CONVERTION)
     server_screen_bin = soc.recv(data_size)
     server_screen_tuple = bin_to_tuple(server_screen_bin)
     client_screen = (screeninfo.get_monitors()[0].width, screeninfo.get_monitors()[0].height)
@@ -55,28 +88,28 @@ def mouse_controle():
     width_ratio = client_screen[0]/server_screen_tuple[0]
     height_ratio = client_screen[1]/server_screen_tuple[1]
     while True:
-        to_do = soc.recv(CHAR_SIZE_IN_BYTES).decode("utf-8")
+        to_do = soc.recv(CHAR_SIZE_IN_BYTES).decode(STR_CONVERTION)
         
         if to_do == 'm':
-            data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), "big")
+            data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), INT_CONVERTION)
             server_position_bin = soc.recv(data_size)
             server_position_tuple = bin_to_tuple(server_position_bin)
             client_position = (server_position_tuple[0]*width_ratio, server_position_tuple[1]*height_ratio)
             client_mouse.position = client_position
         elif to_do == 'c':
-            data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), "big")
-            to_do = soc.recv(data_size).decode("utf-8")
+            data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), INT_CONVERTION)
+            to_do = soc.recv(data_size).decode(STR_CONVERTION)
             
             if to_do == "lp":
-                client_mouse.press(Button.left)
+                client_mouse.press(pynput.mouse.Button.left)
             elif to_do == "rp":
-                client_mouse.press(Button.right)
+                client_mouse.press(pynput.mouse.Button.right)
             elif to_do == "lr":
-                client_mouse.release(Button.left)
+                client_mouse.release(pynput.mouse.Button.left)
             elif to_do == "rr":
-                client_mouse.release(Button.right)
+                client_mouse.release(pynput.mouse.Button.right)
         elif to_do == 's':
-            data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), "big")
+            data_size = int.from_bytes(soc.recv(INT_SIZE_IN_BYTES), INT_CONVERTION)
             scroll_info_bin = soc.recv(data_size)
             scroll_info_tuple = bin_to_tuple(scroll_info_bin)
             dx = scroll_info_tuple[0]
@@ -85,7 +118,8 @@ def mouse_controle():
             client_mouse.scroll(dx, dy)
 
 def bin_to_tuple(bin_data):
-    str_data = bin_data.decode("utf-8")
+    global STR_CONVERTION
+    str_data = bin_data.decode(STR_CONVERTION)
     str_tuple_data = str_data.split(',')
     int_tuple_data = string_tuple_to_int_tuple(str_tuple_data)
     return int_tuple_data
@@ -98,4 +132,16 @@ def string_tuple_to_int_tuple(str_tup):
         i += 1
     return int_tup
 
-mouse_controle()
+def computer_controle():
+    threading.Thread(target=mouse_controle).start()
+    threading.Thread(target=keyboard_controle).start()
+    threading.Thread(target=share_screen).start()
+
+root = tk.Tk()
+root.geometry("500x375")
+button = tk.Button(root,
+                   text="start",
+                   command=computer_controle)
+button.pack()
+
+root.mainloop()
